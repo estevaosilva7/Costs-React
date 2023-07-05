98709e1c2f5516c9e148d51787243fb1622862bd
Estevão <estevaosilva.sg@gmail.com>
2023-07-05 17:20:30 -0300

    Removendo projetos

--- costs/src/components/pages/Projects.js
+++ costs/src/components/pages/Projects.js
@@ -5,12 +5,13 @@ import Message from "../layout/Message"
 import styles from './Projects.module.css'
 import LinkButton from '../layout/LinkButton'
 import Container from '../layout/Container'
-import Loanding from '../layout/Loading'
+import Loading from '../layout/Loading'
 import ProjectCard from '../project/ProjectCard'
 
 function Projects() {
     const [projects, setProjects] = useState([])
     const [removeLoading, setRemoveLoading] = useState(false)
+    const [projectMessage, setProjectMessage] = useState('') // Correção: Definir o estado inicial corretamente
     const location = useLocation()
     let message = ''
     if (location.state) {
@@ -18,22 +19,37 @@ function Projects() {
     }
 
     useEffect(() => {
-     setTimeout(() => {
-        fetch('http://localhost:5000/projects', {
-            method: 'GET',
+        setTimeout(() => {
+            fetch('http://localhost:5000/projects', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },  
+            })
+                .then((resp) => resp.json())
+                .then((data) => {
+                    console.log(data)
+                    setProjects(data)
+                    setRemoveLoading(true)
+                })
+                .catch((err) => console.log(err))
+        }, 300);
+    }, [])
+
+    function removeProject(id) { 
+
+        fetch(`http://localhost:5000/projects/${id}`, {
+            method: 'DELETE',
             headers: {
-              'Content-Type': 'application/json',
-            },  
-          })
-              .then((resp) => resp.json())
-              .then((data) => {
-                  console.log(data)
-                  setProjects(data)
-                  setRemoveLoading(true)
-              })
-              .catch((err) => console.log(err))
-     }, 300);
-    },[])
+                'Content-Type': 'application/json'
+            },
+        }).then(resp => resp.json())
+        .then(() => {
+            setProjects(projects.filter((project) => project.id !== id))
+            setProjectMessage('Projeto removido com sucesso!') 
+        })
+        .catch(err => console.log(err))
+    }
 
     return (
         <div className={styles.project_container}>
@@ -42,6 +58,7 @@ function Projects() {
                 <LinkButton to="/newproject" text="Criar Projeto"/>
             </div>
             {message && <Message type="success" msg={message} />}
+            {projectMessage && <Message type="success" msg={projectMessage} />}
             <Container  customClass="start">
                 {projects.length > 0 &&
                     projects.map((project) => (
@@ -51,12 +68,13 @@ function Projects() {
                             budget={project.budget}
                             category={project?.category?.name}
                             key={project.id}
+                            handleRemove={removeProject} 
                         />
                     ))}
-                    {!removeLoading && <Loanding />}
-                    {removeLoading && projects.length === 0 && (
-                        <p> Não há projetos cadastrados!</p>
-                    )}
+                {!removeLoading && <Loading />} {}
+                {removeLoading && projects.length === 0 && (
+                    <p> Não há projetos cadastrados!</p>
+                )}
             </Container>
         </div>
     )
