a8fc1eb1a8159216816450917020578d9681b9ce
Estevão <estevaosilva.sg@gmail.com>
2023-07-06 17:39:11 -0300

    Página de edição de dados

--- costs/src/App.js
+++ costs/src/App.js
@@ -4,6 +4,7 @@ import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
 import Projects from './components/pages/Projects';
+import Project from './components/pages/Project';
 
 import Container from './components/layout/Container';
 import Navbar from './components/layout/Navbar'
@@ -22,6 +23,7 @@ function App() {
                 <Route exact path='/company' element={<Company />} />
                 <Route exact path='/contact' element={<Contact />} />
                 <Route exact path='/newproject' element={<NewProject />} />
+                <Route exact path='/project/:id' element={<Project />} />
               </Routes>
           </Container>    
         <Footer/>
