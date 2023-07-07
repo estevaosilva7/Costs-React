71fb777aa1c2bc0877803f0398ffe9536c479ae5
Estevão <estevaosilva.sg@gmail.com>
2023-07-07 17:13:19 -0300

    Exibindo detalhes do projeto

--- costs/src/components/pages/Project.js
+++ costs/src/components/pages/Project.js
@@ -1,34 +1,64 @@
 import styles from './Project.module.css'
+import Loading from '../layout/Loading'
+import Container from '../layout/Container'
 
 import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
+
 
 function Project() {
     const { id } = useParams()
 
     const [project, setProject] = useState([])
+    const [ showProjectForm, setShowProjectForm] = useState(false)
 
     useEffect(() => {
-        fetch(`http://localhost:5000/projects/${id}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-        .then((resp) => resp.json())
-        .then((data) => {
-            setProject(data)
-        })
-        .catch((err) => console.log)
+        setTimeout(() => {
+            fetch(`http://localhost:5000/projects/${id}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+            .then((resp) => resp.json())
+            .then((data) => {
+                setProject(data)
+            })
+            .catch((err) => console.log)
+        },300)
     }, [id])
 
+    function toggleProjectForm() {
+        setShowProjectForm(!showProjectForm)
+    }
+
     return (
+    <>
+    {project.name ? (
         <div>
-            
-                 <p>{project.name}</p>
-             
+            <Container customClass="column">
+                <div>
+                    <h1> Projeto: {project.name} </h1>
+                    <button onClick={toggleProjectForm}> 
+                        {!showProjectForm ? 'Editar projeto' : 'Fechar'} 
+                    </button>
+                    {!showProjectForm ? (
+                        <div>
+                            <p>project setShowProjectForm</p>
+                        </div>
+                    ) : (
+                        <div>
+                            <p>detalhes do projeto</p>
+                        </div>
+                    )}
+                </div>
+            </Container>
         </div>
-     )
+    ): (
+        <Loading />
+    )}
+   </>
+   )
 }
 
 export default Project
