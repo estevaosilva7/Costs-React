9fc4beb09153591f68725002578183c01f57841b
Estevão <estevaosilva.sg@gmail.com>
2023-07-11 16:02:53 -0300

    Adicionando novos serviços 2

--- costs/src/components/service/ServiceForm.js
+++ costs/src/components/service/ServiceForm.js
@@ -1,8 +1,39 @@
-function ServiceForm() {
-    return(
-        <form action=""> 
-        <input type="text" />
+import {useState} from 'react'
 
+import Input from '../form/Input'
+import SubmitButton from '../form/SubmitButton'
+
+import styles from '../project/ProjectForm.module.css'
+
+function ServiceForm({ handleSubmit, btnText, projectData}) {
+    function submit() {}
+
+    function handleChange(e) {}
+
+    return(
+        <form onSubmit={submit} className={styles.form}> 
+        <Input
+        type="text"
+        text="Nome do serviço"
+        name="name"
+        placeholder="Insira o nome do serviço"
+        handleOnChange={handleChange}
+        />
+        <Input
+        type="number"
+        texte="Custo do serviço"
+        name="cost"
+        placeholder="Insira o valor total"
+        handleOnChange={handleChange}
+        />
+        <Input
+        type="text"
+        text="Descrição do serviço"
+        name="description"
+        placeholder="Descreva o serviço"
+        handleOnChange={handleChange}
+        />
+        <SubmitButton text={btnText} />
         </form>
     )
 }
